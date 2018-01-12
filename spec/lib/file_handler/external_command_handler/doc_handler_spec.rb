4218b352eaea56692150ef7c4e0fe8d6245d5729
Wieland Lindenthal <w.lindenthal@forkmerge.com>
2018-01-12 15:48:13 +0100

    Add spec for DocHandler

---
+++ spec/lib/file_handler/external_command_handler/doc_handler_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe TextExtractor::DocHandler do
+
+  subject { described_class.new }
+
+  if TextExtractor::DocHandler.available?
+    it 'Should extract text from .doc files' do
+      file = File.new('spec/fixtures/files/text.doc', 'r')
+
+      expect(subject.text(file, 'application/vnd.ms-word')).to match /lorem ipsum fulltext find me!/
+      expect(subject.text(file, 'application/msword')).to /match lorem ipsum fulltext find me!/
+    end
+  else
+    warn "DocHanlder could not be tested as external program is not available."
+  end
+end
