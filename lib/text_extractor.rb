737f2362d12deb54fe9dfd59c32d832a297db578
Wieland Lindenthal <w.lindenthal@forkmerge.com>
2018-01-12 14:26:45 +0100

    extract module CodesetUtil

--- lib/text_extractor.rb
+++ lib/text_extractor.rb
@@ -3,6 +3,8 @@
 require 'text_extractor/version'
 require 'text_extractor/configuration'
 
+require 'text_extractor/codeset_util'
+
 require 'text_extractor/file_handler'
 require 'text_extractor/file_handler/external_command_handler'
 require 'text_extractor/file_handler/external_command_handler/doc_handler'
@@ -21,34 +23,3 @@ require 'text_extractor/file_handler/zipped_xml_handler/opendocument_handler'
 require 'text_extractor/file_handler/plaintext_handler'
 
 require 'text_extractor/resolver'
-
-# require 'text_extractor/file_handler/plaintext_handler'
-# require 'text_extractor/resolver'
-
-module TextExtractor
-  module CodesetUtil
-    def self.to_utf8(str, encoding)
-      return str if str.nil?
-      str.force_encoding('ASCII-8BIT')
-      if str.empty?
-        str.force_encoding('UTF-8')
-        return str
-      end
-      enc = (encoding.nil? || encoding.size == 0) ? 'UTF-8' : encoding
-      if enc.upcase != 'UTF-8'
-        str.force_encoding(enc)
-        str = str.encode('UTF-8', invalid: :replace,
-                         undef: :replace, replace: '?')
-      else
-        str.force_encoding('UTF-8')
-        if !str.valid_encoding?
-          str = str.encode('US-ASCII', invalid: :replace,
-                           undef: :replace, replace: '?').encode('UTF-8')
-        end
-      end
-      str
-    end
-  end
-
-end
-
