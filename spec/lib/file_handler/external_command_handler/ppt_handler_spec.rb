5990eb52180bd8169b875f82ed39e6af11adca1a
Wieland Lindenthal <w.lindenthal@forkmerge.com>
2018-01-17 12:31:21 +0100

    Adding an old ppt file for spec with which catppt doesn’t have problems.

--- spec/lib/file_handler/external_command_handler/ppt_handler_spec.rb
+++ spec/lib/file_handler/external_command_handler/ppt_handler_spec.rb
@@ -10,9 +10,9 @@ describe TextExtractor::PptHandler do
     it 'Should extract text from .ppt files' do
       file = File.new('spec/fixtures/files/presentation.ppt', 'r')
 
-      expect(subject.text(file)).to match /The title/
-      expect(TextExtractor::Resolver.new(file, 'application/powerpoint').text).to match /The Title find me Slide two/
-      expect(TextExtractor::Resolver.new(file, 'application/vnd.ms-powerpoint').text).to match /The Title find me Slide two/
+      expect(subject.text(file)).to match /Die Java Virtual Machine/
+      expect(TextExtractor::Resolver.new(file, 'application/powerpoint').text).to match /Die Java Virtual Machine/
+      expect(TextExtractor::Resolver.new(file, 'application/vnd.ms-powerpoint').text).to match /Die Java Virtual Machine/
     end
   else
     warn "#{described_class.name} could not be tested as external program is not available."
