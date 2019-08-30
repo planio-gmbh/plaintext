59759139aa279dcd318ad00f651f804db1e166aa
Oliver Günther <mail@oliverguenther.de>
2019-08-30 08:08:17 +0200

    Ensure PDF extraction is read as UTF-8 stream
    The pdftotext options specify that the internal PDF encoding should be
    converted into UTF-8. Thus `pdftotext` will stream UTF-8 encoded data.
    
    The `ExternalCommandHandler` however always reads ASCII-8BIT, which
    results in wrong transcoding into UTF-8 when read from the PDF handler.
    
    This PR extends the command handler to specify whether the stream is
    UTF-8 and avoid conversion appropriately.
    
    Original bug report with the test.pdf used as fixture now
    https://community.openproject.com/wp/30891

--- lib/plaintext/file_handler/external_command_handler/pdf_handler.rb
+++ lib/plaintext/file_handler/external_command_handler/pdf_handler.rb
@@ -5,9 +5,16 @@ module Plaintext
     DEFAULT = [
         '/usr/bin/pdftotext', '-enc', 'UTF-8', '__FILE__', '-'
     ].freeze
+
     def initialize
       @content_type = 'application/pdf'
       @command = Plaintext::Configuration['pdftotext'] || DEFAULT
+    end
+
+    protected
+
+    def utf8_stream?
+      true
     end
   end
 end
--- spec/lib/file_handler/external_command_handler/pdf_handler_spec.rb
+++ spec/lib/file_handler/external_command_handler/pdf_handler_spec.rb
@@ -13,6 +13,13 @@ describe Plaintext::PdfHandler do
       expect(subject.text(file)).to match /lorem ipsum fulltext find me!/
       expect(Plaintext::Resolver.new(file, 'application/pdf').text).to match /lorem ipsum fulltext find me!/
     end
+
+    it 'should extract umlauts correctly into UTF-8' do
+      file = File.new('spec/fixtures/files/text-with-umlaut.pdf', 'r')
+
+      expect(subject.text(file)).to match /In der Küche hat es eine Kaffeemaschine/
+      expect(Plaintext::Resolver.new(file, 'application/pdf').text).to match /In der Küche hat es eine Kaffeemaschine/
+    end
   else
     warn "#{described_class.name} could not be tested as external program is not available."
   end
